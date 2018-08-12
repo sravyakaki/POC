30e0ca55ee1b45423f1ca2c9f06512fb9f860239
Kaki, Sravya (Accenture) <skaki@allstate.com>
2018-08-11 20:40:14 -0400

    Adding plist file downloaded from Firebase after creating an account

---
+++ sk-socialnet/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  sk-socialnet
+//
+//  Copyright © 2018 Sravya Kaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
