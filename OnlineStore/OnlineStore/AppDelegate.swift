e258102a64465331bf37ff65cfe7aa02260e58a0
Kaki, Sravya (Accenture) <skaki@allstate.com>
2018-08-19 16:36:15 -0400

    Signature

--- OnlineStore/OnlineStore/AppDelegate.swift
+++ OnlineStore/OnlineStore/AppDelegate.swift
@@ -2,9 +2,6 @@
 //  AppDelegate.swift
 //  OnlineStore
 //
-//  Created by Kaki, Sravya (Accenture) on 8/19/18.
-//  Copyright © 2018 Sravya Kaki. All rights reserved.
-//
 
 import UIKit
 
--- OnlineStore/OnlineStore/ViewController.swift
+++ OnlineStore/OnlineStore/ViewController.swift
@@ -2,9 +2,6 @@
 //  ViewController.swift
 //  OnlineStore
 //
-//  Created by Kaki, Sravya (Accenture) on 8/19/18.
-//  Copyright © 2018 Sravya Kaki. All rights reserved.
-//
 
 import UIKit
 
