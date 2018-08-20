ccb79eac2ff6270061cf44ce97daaccdb43df006
Kaki, Sravya (Accenture) <skaki@allstate.com>
2018-08-19 23:32:48 -0400

    Modify author name

--- OnlineStore/OnlineStore/Views/CategoryCell.swift
+++ OnlineStore/OnlineStore/Views/CategoryCell.swift
@@ -2,7 +2,7 @@
 //  CategoryCell.swift
 //  OnlineStore
 //
-//  Created by Kaki, Sravya (Accenture) on 8/19/18.
+//  Created by Kaki, Sravya  on 8/19/18.
 //  Copyright © 2018 Sravya Kaki. All rights reserved.
 //
 
