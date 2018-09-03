d3129b084ec1013ddd7cefa81e0e740d46c037a3
Kaki, Sravya (Accenture) <skaki@allstate.com>
2018-09-03 18:23:16 -0400

    Adding Navigation controller

---
+++ OnlineStore/OnlineStore/Views/ProductCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductCell.swift
+//  OnlineStore
+//
+//  Created by Kaki, Sravya (Accenture) on 9/3/18.
+//  Copyright © 2018 Sravya Kaki. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
